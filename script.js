a5a657061499e0d4688fac3dfb44a1d3ce9be0d4
Charan Reddy <154213087+KUNAMMANISAICHARANREDDY@users.noreply.github.com>
2025-08-21 17:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function mincost(arr)
 { 
 //write your code here
+	Arrays.sort(arr);
+	let a[];
+	a[0]=a[0]+a[1];
+	for(int i=2;i<arr.length;i++)
+	{
+		a[i-1]=a[i-2]+arr[i];
+	}
+	let mincost=a.reduce((acc,n)=> acc+n,0);
+	return mincost;
 // return the min cost
   
 }
