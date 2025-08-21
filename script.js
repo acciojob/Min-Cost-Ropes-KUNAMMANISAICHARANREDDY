bb3aeb0068460363b7d64b6dd6da4b55125823ed
Charan Reddy <154213087+KUNAMMANISAICHARANREDDY@users.noreply.github.com>
2025-08-21 17:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-function mincost(arr)
-{ 
-//write your code here
-	Arrays.sort(arr);
-	let a[];
-	a[0]=a[0]+a[1];
-	for(int i=2;i<arr.length;i++)
-	{
-		a[i-1]=a[i-2]+arr[i];
-	}
-	let mincost=a.reduce((acc,n)=> acc+n,0);
-	return mincost;
-// return the min cost
-  
+function mincost(arr) {
+    // sort array initially
+    arr.sort((a, b) => a - b);
+
+    let cost = 0;
+
+    // keep combining until one element remains
+    while (arr.length > 1) {
+        // take two smallest
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let sum = first + second;
+        cost += sum;
+
+        // insert sum back in sorted order
+        let i = 0;
+        while (i < arr.length && arr[i] < sum) i++;
+        arr.splice(i, 0, sum);
+    }
+
+    return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
+
